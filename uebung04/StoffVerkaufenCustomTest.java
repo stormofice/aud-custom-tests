32e19f91e370bded90cf69c2767b6d1a7ba438bb
BuildTools <58337328+stormofice@users.noreply.github.com>
2020-12-04 14:27:15 +0100

    Added tests for 'uebung04/StoffVerkaufen'

--- uebung04/StoffVerkaufenCustomTest.java
+++ uebung04/StoffVerkaufenCustomTest.java
@@ -41,4 +41,45 @@ public class StoffVerkaufenCustomTest extends StoffVerkaufenPublicTest {
                 "You should remove any such calls before upload!", "", errContent.toString());
         System.setErr(originalErr);
     }
+
+    @Test(timeout = 4200)
+    public void customTest_verkaufenNaive() {
+        long[] preise_custom = {};
+        check_naive("custom_1", -1, preise_custom, 0, 1);
+        check_naive("custom_2", 0, preise_custom, 0, 1);
+
+        preise_custom = new long[] {1};
+        check_naive("custom_3", -1, preise_custom, 0, 1);
+        check_naive("custom_4", 0, preise_custom, 0, 1);
+        check_naive("custom_5", 1, preise_custom, 1, 3);
+        check_naive("custom_6", 420, preise_custom, 420, 88831);
+
+        preise_custom = new long[] {0};
+        check_naive("custom_7", -1, preise_custom, 0, 1);
+        check_naive("custom_8", 0, preise_custom, 0, 1);
+        check_naive("custom_9", 1, preise_custom, 0, 3);
+        check_naive("custom_10", 420, preise_custom, 0, 88831);
+    }
+
+    @Test(timeout = 420)
+    public void customTest_verkaufenDP() {
+        long[] preise_custom = {};
+        check_dp("custom_11", -1, preise_custom, 0);
+        check_dp("custom_12", 0, preise_custom, 0);
+        check_dp("custom_13", 1, preise_custom, 0);
+        check_dp("custom_14", 420, preise_custom, 0);
+
+        preise_custom = new long[] {1};
+        check_dp("custom_15", -1, preise_custom, 0);
+        check_dp("custom_16", 0, preise_custom, 0);
+        check_dp("custom_17", 1, preise_custom, 1);
+        check_dp("custom_18", 420, preise_custom, 420);
+
+        preise_custom = new long[] {0};
+        check_dp("custom_19", -1, preise_custom, 0);
+        check_dp("custom_20", 0, preise_custom, 0);
+        check_dp("custom_21", 1, preise_custom, 0);
+        check_dp("custom_22", 420, preise_custom, 0);
+    }
+
 }
